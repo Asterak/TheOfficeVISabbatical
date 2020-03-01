cdfd1c3e5134d46a3f1c13827bdfcb9da684c494
Сергей Гупалов <asterak@gmail.com>
2020-03-01 20:06:21 +0300

    Add project files.

---
+++ TheOfficeVISabbatical/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TheOfficeVISabbatical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sabb("Can I have a sabbatical?", 5, 5)); //"Sabbatical! Boom!"
+            Console.WriteLine(Sabb("Why are you shouting?", 7, 2)); //"Back to your desk, boy."
+            Console.WriteLine(Sabb("What do you mean I cant learn to code??", 8, 9));//"Sabbatical! Boom!"
+            Console.WriteLine(Sabb("Please calm down", 9, 1)); //"Back to your desk, boy."
+        }
+
+        public static string Sabb(string x, int val, int happ)
+        {
+            return (Regex.Matches(x.ToLower(), @"[sabbatical]").Count() + val + happ) > 22 ? "Sabbatical! Boom!" : "Back to your desk, boy.";
+        }
+    }
+}
